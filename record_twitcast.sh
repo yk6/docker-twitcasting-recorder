b625f64fc5184d993bd366739c2447248137199c
陳鈞 <jim60105@gmail.com>
2021-07-02 12:12:19 +0800

    修改log，重啟m3u8錄影為備援

--- record_twitcast.sh
+++ record_twitcast.sh
@@ -21,12 +21,11 @@ while true; do
 
   # Record using MPEG-2 TS format to avoid broken file caused by interruption
   FNAME="twitcast_${1}_$(date +"%Y%m%d_%H%M%S").ts"
-  echo "$LOG_PREFIX Start recording, stream saved to \"$FNAME\"."
-  echo "$LOG_PREFIX Use command \"tail -f $FNAME.log\" to track recording progress."
+  echo "$LOG_PREFIX Start recording..."
 
-  # # Also record low resolution stream simultaneously as backup
-  # M3U8_URL="http://twitcasting.tv/$1/metastream.m3u8?video=1"
-  # ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "/download/m3u8_$FNAME" &
+  # Also record low resolution stream simultaneously as backup
+  M3U8_URL="http://twitcasting.tv/$1/metastream.m3u8?video=1"
+  ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "/download/m3u8_$FNAME" &
 
   # Start recording
   docker run --rm --name "record_livedl" -v "${ARCHIVE}:/livedl" ghcr.io/jim60105/livedl:my-docker-build "https://twitcasting.tv/$1" -tcas tcas-retry=on -tcas-retry-interval=30
