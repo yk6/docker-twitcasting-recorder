4ac37e794a42c82e21ac6cff769fd66df864ca6f
陳鈞 <jim60105@gmail.com>
2021-07-23 19:51:06 +0800

    Add Convert ts to mp4, modify logging

--- record_twitcast.sh
+++ record_twitcast.sh
@@ -20,19 +20,23 @@ while true; do
   done
 
   # Record using MPEG-2 TS format to avoid broken file caused by interruption
-  FNAME="twitcast_${1}_$(date +"%Y%m%d_%H%M%S").ts"
+  FNAME="twitcast_${1}_$(date +"%Y%m%d_%H%M%S")"
   echo "$LOG_PREFIX Start recording..."
 
   # Also record low resolution stream simultaneously as backup
   M3U8_URL="http://twitcasting.tv/$1/metastream.m3u8?video=1"
-  ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "/download/m3u8_$FNAME" &
+  ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "/download/m3u8_${FNAME}.ts" &
 
   # Start recording
   # docker run --rm --name "record_livedl" -v "${ARCHIVE}:/livedl" ghcr.io/jim60105/livedl:my-docker-build "https://twitcasting.tv/$1" -tcas -tcas-retry=on -tcas-retry-interval 30
-  python /main.py --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" -o "/download/ws_$FNAME" $1
+  python /main.py --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" -o "/download/ws_${FNAME}.ts" $1
+  LOG_PREFIX=$(date +"[%m/%d/%y %H:%M:%S] [twitcasting@$1] ")
+  echo "$LOG_PREFIX Stop recording ${FNAME}"
+
+  # Convert to mp4
+  echo "$LOG_PREFIX Start convert ws_${FNAME}.ts to mp4..."
+  ffmpeg -i "/download/ws_${FNAME}.ts" -c copy -movflags +faststart "/download/${FNAME}.mp4" &
 
   # Exit if we just need to record current stream
-  LOG_PREFIX=$(date +"[%m/%d/%y %H:%M:%S] [twitcasting@$1] ")
-  echo "$LOG_PREFIX Live stream recording stopped."
   [[ "$2" == "once" ]] && break
 done
